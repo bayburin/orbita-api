fe6f9a1fc9e52e7ff9797caf6c11f163b1a83fb7
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-10-22 17:25:25 +0700

    Изменен класс-обработчик ActionEvent. Теперь он проверяет наличие работы, создает работу и подключает в нее исполнителя при необходимости. Также создает запись в модели Workflow.

--- app/services/events/action_event.rb
+++ app/services/events/action_event.rb
@@ -1,10 +1,38 @@
 module Events
+  # Класс-обработчик события выполнения некоторого действия во внешней системе (аналог workflow в текущей системе).
   class ActionEvent
     include Interactor
 
+    delegate :event, :work, :workflow, to: :context
+
     def call
-      p 'ActionEvent.call'
-      # TODO: Создать запись в таблице messages с сообщением из payload. Добавить запись в таблицу histories.
+      # Проверка существования заявки и пользователя
+      claim = Claim.find(event.claim_id)
+      user = User.find_by(id_tn: event.id_tn)
+      context.fail!(error: 'Пользователь не найден') unless user
+
+      Work.transaction do
+        # Проверка наличия работы и ее создания
+        context.work = claim.works.where(group_id: user.group_id).first_or_create do |w|
+          w.group_id = user.group_id
+        end
+        # Подключение полученного пользователя к работе
+        # TODO: На этом этапе должно логироваться подключение нового пользователя, если его раньше не было в работе по заявке
+        work.users << user
+
+        # Создание записи workflow
+        context.workflow = work.workflows.build(
+          message: event.payload['message'],
+          sender: user,
+          claim: claim
+        )
+
+        if workflow.save
+          context.message = 'successfully created'
+        else
+          context.fail!(error: workflow.errors.full_messages)
+        end
+      end
     end
   end
 end
