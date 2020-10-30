059fd63c15a6bc48dbff571914a0e2d1ea6ada16
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-10-30 09:41:44 +0700

    Исправлена миграция

--- db/migrate/20201026042618_rename_action_template.rb
+++ db/migrate/20201026042618_rename_action_template.rb
@@ -2,8 +2,17 @@ class RenameActionTemplate < ActiveRecord::Migration[6.0]
   def change
     rename_table :action_templates, :event_types
     add_column :event_types, :description, :string, after: :name
-    change_column :event_types, :name, :integer, limit: 1, index: true
     remove_column :histories, :action_type, :integer, limit: 1
     add_reference :histories, :event_type, foreign_key: true, after: :id
+
+    reversible do |change|
+      change.up do
+        change_column :event_types, :name, :integer, limit: 1, index: true
+      end
+
+      change.down do
+        change_column :event_types, :name, :string, index: true
+      end
+    end
   end
 end
