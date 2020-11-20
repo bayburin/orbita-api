16a03e9116b5117e9a36cb7a4df38f4c8fd41c8d
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-11-20 16:11:53 +0700

    В CaseSerializer добавлен атрибут type.

--- app/serializers/claim_serializer.rb
+++ app/serializers/claim_serializer.rb
@@ -1,10 +1,10 @@
 class ClaimSerializer < ActiveModel::Serializer
-  attributes :id, :status, :priority, :attrs, :runtime
+  attributes :id, :type, :status, :priority, :attrs, :runtime
 
   has_many :works
   has_one :source_snapshot
 
   def runtime
-    ActiveModelSerializers::SerializableResource.new(object.runtime, root: 'runtime').serializable_hash
+    RuntimeSerializer.new(object.runtime).as_json
   end
 end
