7be168fb90b48ca9c75d5356ebac64c1b2fafb0a
Байбурин Равиль <baiburinrf@gmail.com>
2021-04-02 20:41:02 +0700

    В ApplicationRecord добавлен метод ::translate_enum для перевода enum-объектов. Обработчик формы UpdateForm теперь сохраняет историю с переведенным приоритетом.

--- app/forms/sd_requests/update_form.rb
+++ app/forms/sd_requests/update_form.rb
@@ -9,7 +9,7 @@ module SdRequests
         history_store.add(Histories::PostponeType.new(datetime: datetime.finished_at_plan_str).build)
       end
 
-      history_store.add(Histories::PriorityType.new(priority: priority).build) if changed?(:priority)
+      history_store.add(Histories::PriorityType.new(priority: Claim.translate_enum(:priority, priority)).build) if changed?(:priority)
       super
     end
   end
