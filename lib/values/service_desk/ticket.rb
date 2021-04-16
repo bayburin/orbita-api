3c62373ccab7b9c3b366e47ec26e35ee791c802f
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-04-16 11:12:22 +0700

    В класс ServiceDesk::Ticket добавлен атрибут id.

--- lib/values/service_desk/ticket.rb
+++ lib/values/service_desk/ticket.rb
@@ -3,6 +3,7 @@ module ServiceDesk
   class Ticket < Dry::Struct
     transform_keys(&:to_sym)
 
+    attribute :id, Types::Integer.optional
     attribute :identity, Types::Integer.optional
     attribute :name, Types::String.optional
     attribute :service, Types::SdService.optional
