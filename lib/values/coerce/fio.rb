61d8e654a36f94a9c00d743039502320d6294faa
Байбурин Равиль <baiburinrf@gmail.com>
2021-04-19 21:07:23 +0700

    Удален класс Fio.

--- lib/values/coerce/fio.rb
+++
@@ -1,9 +0,0 @@
-# Класс для обработки ФИО.
-# TODO: Удалить класс
-class Fio < Virtus::Attribute
-  def coerce(value)
-    return value unless value.is_a?(String)
-
-    value.split(' ').map { |str1| str1.split('-').map { |str2| str2[0].upcase + str2[1..-1].downcase }.join('-') }.join(' ')
-  end
-end
