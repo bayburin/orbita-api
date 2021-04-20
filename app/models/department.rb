a7cf122428d85961e3d9d30a2ebac5021225b358
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-04-20 17:20:54 +0700

    В модель Department добавлена связь :users.

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,4 @@
 class Department < ApplicationRecord
   has_many :groups, dependent: :destroy
+  has_many :users, through: :groups
 end
--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Department, type: :model do
   it { is_expected.to have_many(:groups) }
+  it { is_expected.to have_many(:users).through(:groups) }
 end
