cddf2540c7c749d5d55be380d05ede5ec572683e
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-04-29 19:32:00 +0700

    Во время авторизации домен у атрибута email теперь не сбрасывается (AuthCenter возвращает email без домена)

--- app/services/auth_center/update_user.rb
+++ app/services/auth_center/update_user.rb
@@ -9,13 +9,14 @@ module AuthCenter
       user = User.find_by(id_tn: user_info['id_tn'])
       context.fail!(message: 'Доступ запрещен') unless user
 
+      email = user_info['email'].to_s.empty? ? nil : "#{user_info['email']}@iss-reshetnev.ru"
       if user.update(
         tn: user_info['tn'],
         fio: user_info['fio'],
         work_tel: user_info['tel'],
-        email: user_info['email']
+        email: email
       )
-        context.user = user
+        context.user = user.reload
         context.user.auth_center_token = AuthCenterToken.new(auth_data)
       else
         context.fail!(message: user.errors.full_messages)
--- spec/services/auth_center/update_user_spec.rb
+++ spec/services/auth_center/update_user_spec.rb
@@ -2,42 +2,45 @@ require 'rails_helper'
 
 module AuthCenter
   RSpec.describe UpdateUser do
-    let!(:user) { create(:admin) }
-    let(:user_params) do
+    let!(:user) { create(:admin, email: 'test') }
+    let!(:user_params) do
       {
         id_tn: user.id_tn,
         tn: user.tn + 1,
-        fio: user.fio + 'new',
-        tel: user.work_tel + 'new',
+        fio: user.fio + 'new fio',
+        tel: user.work_tel + 'new tel',
         email: user.email + 'new'
       }
     end
     let(:auth_data) { build(:auth_center_token) }
-    let(:params) { { user_info: user, auth_data: auth_data } }
+    let(:params) { { user_info: user_params.as_json, auth_data: auth_data } }
     subject(:context) { described_class.call(params) }
 
     describe '.call' do
-      it 'finished with success' do
-        expect(context).to be_a_success
-      end
+      it { expect(context).to be_a_success }
 
       context 'when user not found' do
         before { allow(User).to receive(:find_by).and_return(nil) }
 
-        it 'finished with fail' do
-          expect(context).to be_a_failure
-        end
-
-        it 'set message to context' do
-          expect(context.message).to eq 'Доступ запрещен'
-        end
+        it { expect(context).to be_a_failure }
+        it { expect(context.message).to eq 'Доступ запрещен' }
       end
 
-      it 'update user data' do
-        context.user.tn = user_params[:tn]
-        context.user.fio = user_params[:fio]
-        context.user.work_tel = user_params[:tel]
-        context.user.email = user_params[:email]
+      it { expect(context.user.tn).to eq user_params[:tn] }
+      it { expect(context.user.fio).to eq user_params[:fio] }
+      it { expect(context.user.work_tel).to eq user_params[:tel] }
+      it { expect(context.user.email).to eq "#{user_params[:email]}@iss-reshetnev.ru" }
+
+      context 'when email is nil' do
+        before { user_params['email'] = nil }
+
+        it { expect(context.user.email).to be_nil }
+      end
+
+      context 'when email is empty string' do
+        before { user_params['email'] = '' }
+
+        it { expect(context.user.email).to be_nil }
       end
 
       context 'when user was not updated' do
