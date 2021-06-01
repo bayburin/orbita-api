53da813ce0e13f4108dd336e9cbee2782184a0d0
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-06-01 16:20:32 +0700

    Изменен AuthController, теперь параметр code берется напрямую из полученных параметров, без обработки

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AuthController < Api::V1::BaseController
   skip_before_action :authenticate_user!, only: :token
 
   def token
-    result = Users::Authorize.call(code: auth_params[:code])
+    result = Users::Authorize.call(code: params[:code])
 
     if result.success?
       render json: { token: result.jwt }
@@ -13,11 +13,5 @@ class Api::V1::AuthController < Api::V1::BaseController
 
   def revoke
     # TODO: Необходимо отозвать токен
-  end
-
-  protected
-
-  def auth_params
-    params.require(:auth).permit(:code)
   end
 end
