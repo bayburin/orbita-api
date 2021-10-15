057dd30ab4048e0a83407c06757bfa019fe10f15
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-10-15 14:07:33 +0700

    Исправлен баг: в астрею помимо табельного номера исполнителя теперь отправляется и табельный номер пользователя

--- app/adapters/astraea/new_form_state.rb
+++ app/adapters/astraea/new_form_state.rb
@@ -6,22 +6,6 @@ module Astraea
       @source_snapshot = form.source_snapshot
     end
 
-    def case_id
-      @form.integration_id
-    end
-
-    def phone
-      @source_snapshot.user_attrs[:phone]
-    end
-
-    def host_id
-      @source_snapshot.invent_num
-    end
-
-    def barcode
-      @source_snapshot.barcode
-    end
-
     def desc
       @form.description
     end
